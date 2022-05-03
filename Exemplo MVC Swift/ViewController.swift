12e6f9f27598e012ec7eaf93ef30bbb67ad25dd2
WilliCarvalho <willian_matheus14@hotmail.com>
2022-05-03 08:40:19 -0300

    Added error messages for the user

--- Exemplo MVC Swift/ViewController.swift
+++ Exemplo MVC Swift/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var peso: UITextField!
     @IBOutlet weak var altura: UITextField!
     @IBOutlet weak var imc: UITextField!
+    @IBOutlet weak var userMsg: UILabel!
+    
     var pessoa = DadosPessoa();
 
     
@@ -26,7 +28,25 @@ class ViewController: UIViewController {
         pessoa.nome = nome.text
         pessoa.peso = Float(peso.text!)
         pessoa.altura = Float(altura.text!)
-        imc.text =  "\(pessoa.Calculo2())"
+        
+        if(peso.text == ""){
+            userMsg.text = "Digite algum valor para o peso"
+        }
+        else if(altura.text == ""){
+            userMsg.text = "Digite algum valor para a altura"
+        }
+        else if(imc.text != ""){
+            userMsg.text = "não digite nada neste campo"
+        }
+        else if(peso.text != "" && altura.text != "" && imc.text == ""){
+            //formatação utilizando a função que atribui valor a variável IMC
+            //pessoa.Calculo()
+            //imc.text = String(format: "%.2f", pessoa.imc)
+            //Formatação utilizando a função que retorna um float (utiliza apenas uma linha)
+            imc.text = String(format: "%.2f", pessoa.Calculo2())
+        }
+        
+        
     }
     
 }
